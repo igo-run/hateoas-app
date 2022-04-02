53b191bd6c24e1e055fd4277562481c35a4ba83b
Ihar Kuzniatsou <igo.k8u@gmail.com>
2022-04-02 08:43:28 +0200

    Init project

---
+++ src/main/java/io/igorun/demo/hateoasapp/HateoasAppApplication.java
@@ -0,0 +1,13 @@
+package io.igorun.demo.hateoasapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateoasAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HateoasAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/igorun/demo/hateoasapp/HateoasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.igorun.demo.hateoasapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateoasAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
